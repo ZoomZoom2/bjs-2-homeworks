0e3e545e524047787be9ecdaf3246ba50ae1a74e
ZoomZoom24 <solversoft@yandex.ru>
2023-09-03 04:40:14 +0300

    Шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,53 @@
-﻿
+﻿function parseCount (value) {
+  if (Number.isNaN(Number.parseFloat(value))) {
+    throw new Error('Невалидное значение');
+  }
+  return Number.parseFloat(value);
+}
+
+function validateCount (value) {
+  try {
+    return parseCount(value);
+  } catch (e) {
+    return e;
+  }
+}
+
+class Triangle {
+  constructor (a, b, c) {
+    if ((a + b < c) || (a + c < b) || (b + c < a)) {
+      throw new Error('Треугольник с такими сторонами не существует');
+    }
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  get perimeter () {
+    return this.a + this.b + this.c;
+  }
+
+  get area () {
+    let square = (this.a + this.b - this.c) * (this.a - this.b + this.c) * (-this.a + this.b + this.c) * (this.a + this.b + this.c);
+    square = Number((Math.sqrt(square) / 4).toFixed(3));
+    return square;
+  }
+}
+
+function getTriangle (a, b, c) {
+  let tri = {};
+  try {
+    tri = new Triangle(a, b, c);
+  } catch (e) {
+    tri = {
+      get perimeter () {
+        return 'Ошибка! Треугольник не существует';
+      },
+    
+      get area () {
+        return 'Ошибка! Треугольник не существует';
+      }
+    }
+  }
+  return tri;
+}
